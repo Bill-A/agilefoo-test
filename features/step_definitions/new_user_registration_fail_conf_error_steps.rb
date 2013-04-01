3aaec6070571b594ff03997b85a1a6f1a8e9ee42
Bill Allen <BillAllenEmail@gmail.com>
2013-03-31 23:36:15 -0500

    more Readme updates

--- features/step_definitions/new_user_registration_fail_conf_error_steps.rb
+++
@@ -1,3 +0,0 @@
-Then(/^I land on the New User Registration page with the message 'Password doesn't match confirmation'$/) do
-	page.should have_content("Password doesn't match confirmation")
-end
--- features/step_definitions/new_user_registration_results_steps.rb
+++
@@ -1,4 +0,0 @@
-Then(/^I obtain this response "(.*?)"$/) do 
-	|message|
-	page.should have_content(message)
-end
